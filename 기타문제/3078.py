4b386a5e8edc9d140e5dbe364c525826abbbc83b
lhs961021 <lhs961021@naver.com>
2022-01-24 23:28:24 +0900

    20220124

---
+++ 기타문제/3078.py
@@ -0,0 +1,14 @@
+import sys
+
+N, K = map(int, sys.stdin.readline().split())
+students = [0]*N
+dp = [0]*21
+cnt = 0
+for rank in range(N):
+    l = len(sys.stdin.readline().strip())
+    students[rank] = l
+    if rank > K:
+        dp[students[rank-K-1]] -= 1
+    cnt += dp[l]
+    dp[l] += 1
+print(cnt)
