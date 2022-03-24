1cf179989b0a273ba86aa7add1ba08c2fba715aa
lhs961021 <lhs961021@naver.com>
2022-03-24 15:30:45 +0900

    20220324_third

--- practice/20_분할정복/11444.py
+++ practice/20_분할정복/11444.py
@@ -4,19 +4,19 @@ N = int(sys.stdin.readline())
 
 graph = [[1,1],[1,0]]
 
-def solution(a,b):
+def solution(a,b): #a와 b는 그래프(행렬)
   
   product = [[0]*2 for _ in range(2)]
 
-  for i in range(2):
-    for j in range(2):
-      for k in range(2):
+  for i in range(2): # 2X2 행렬
+    for j in range(2): # 2X2 행렬
+      for k in range(2): # 2X2 행렬
         product[i][j] += a[i][k] * b[k][j]
       product[i][j]%=1000000007
      
-  return product
+  return product #곱한 행렬을 반환
   
-def divide(N,B):
+def divide(N,B): #이분탐색 N 그래프, B는 곱하는 수
   if B==1:
     return graph
   elif B==2:
@@ -25,9 +25,9 @@ def divide(N,B):
     tmp = divide(N,B//2)
     
     if B%2==0:
-      return solution(tmp,tmp)
+      return solution(tmp,tmp) #짝수이면 한번 
     else:
-      return solution(solution(tmp,tmp),graph)
+      return solution(solution(tmp,tmp),graph) #홀수이면 짝수 꺼에 graph한번 더 곱하기
 
 result = divide(graph,N)
 
