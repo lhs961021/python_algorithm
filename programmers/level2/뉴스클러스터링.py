2860d0a5557aef3144459a70f3c9e3d4f3ba55be
lhs961021 <lhs961021@naver.com>
2023-03-17 14:23:03 +0900

    20230317

--- programmers/level2/뉴스클러스터링.py
+++ programmers/level2/뉴스클러스터링.py
@@ -27,3 +27,37 @@ def solution(str1, str2):
         return 65536
     else:
         return int(len(inter) / len(union) * 65536)
+    
+import re
+from collections import Counter
+
+def solution(str1, str2):    
+    str1_list = []
+    str2_list = []
+    
+    for i in range(len(str1)-1):
+        if str1[i:i+2].isalpha():
+             str1_list.append(str1[i:i+2].lower())
+    
+    for i in range(len(str2)-1):
+        if str2[i:i+2].isalpha():
+             str2_list.append(str2[i:i+2].lower())
+    
+    a = (Counter(str1_list) & Counter(str2_list)).values()
+    result_1 = 0
+    for i in a:
+        result_1 += i
+    
+    
+    b = (Counter(str1_list)|Counter(str2_list)).values()
+    result_2 = 0
+    for i in b:
+        result_2 += i
+    
+    try:
+        c = result_1/result_2
+    except:
+        c = 1
+    answer = int(c*65536)
+    
+    return answer
---
+++ programmers/level2/전화번호목록.py
@@ -0,0 +1,11 @@
+def solution(phone_book):
+    dic = {}
+    for i in phone_book:
+        dic[i] = 0
+
+    for i in dic:
+        for j in range(len(i)):
+            if (i[:j] in dic):
+                return False
+
+    return True
