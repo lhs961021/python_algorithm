9555649d2f4075e4693c6e3952efd33c58343573
lhs961021 <lhs961021@naver.com>
2023-03-23 20:50:55 +0900

    20230302

--- programmers/level2/멀쩡한 사각형.py
+++ programmers/level2/멀쩡한 사각형.py
@@ -3,3 +3,16 @@ import math
 def solution(w,h):
     answer = w*h - (w+h-math.gcd(w,h))
     return answer
+
+def solution(w,h):
+    answer = 0
+    if w ==1 or h ==1:
+        return 0
+    
+    if w == h:
+        return sum(range(1,w))*2
+    
+    for i in range(1,w):
+        answer += int((-(h/w)*i)+h)
+        
+    return answer*2
