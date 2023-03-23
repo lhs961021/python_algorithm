6c81084e6da8392704b9582ee1e14cfd92379ebb
lhs961021 <lhs961021@naver.com>
2023-03-23 17:55:22 +0900

    20230323

--- programmers/level2/가장큰수.py
+++ programmers/level2/가장큰수.py
@@ -6,3 +6,18 @@ def solution(numbers):
     answer = str(int(''.join(numbers)))
     
     return answer
+
+def solution(numbers):
+
+    answer = ''
+    
+    numbers = list(map(str, numbers))
+
+    numbers.sort(reverse = True, key = lambda x: (x*4)[:4])
+    
+    answer = ''.join(numbers)
+    
+    if answer[0]=='0':
+        return '0'
+    
+    return answer
